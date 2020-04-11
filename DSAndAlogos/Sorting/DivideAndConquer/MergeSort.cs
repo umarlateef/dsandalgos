17b123efe3588a008ba82c9216e7f446a9138e9c
ulateef <ulateef@vpayusa.com>
2020-04-11 15:17:00 -0500

    Second Push for testing

---
+++ DSAndAlogos/Sorting/DivideAndConquer/MergeSort.cs
@@ -0,0 +1,17 @@
+﻿namespace DSAndAlogos.Sorting.DivideAndConquer
+{
+    class MergeSort
+    {
+        public void Sort(int[] arrayToSort)
+        {
+            
+        }
+
+        private void SortArray(int[] arrayToSort)
+        {
+            //int[] leftHalf;
+            //arrayToSort.(leftHalf,0, );
+            // var leftSortedArray = SortArray()
+        }
+    }
+}
